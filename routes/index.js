890c9dec0b0b304344951f873872ddfff0dd7337
arturr92 <gonart.92@gmail.com>
2021-08-04 23:16:26 -0300

    update

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,69 @@
 import express from 'express';
+import {
+    getInicio,
+    getProductos,
+    postProductos,
+    getCarrito,
+    postCarrito,
+    putCarrito,
+    deleteCarrito,
+    detalleProducto,
+    getOrden,
+    postOrden,
+    putOrden,
+    deleteOrden,
+    getFactura,
+    postFactura,
+    putFactura,
+    deleteFactura,
+    getCliente,
+    postCliente,
+    putCliente,
+    deleteCliente
+} from '../controllers/pathControllers.js';
+
 const router = express.Router();
 
 
+router.get('/', getInicio);
 
-router.get( '/' ,(req, res) => {
-    res.send('Hello World')
-})
+router.get('/productos', getProductos);
 
-router.get( '/productos' ,(req, res) => {
-    res.send('Productos')
-})
+router.post('/productos', postProductos);
 
+router.get('/carrito', getCarrito);
+
+router.put('/carrito', putCarrito);
+
+router.post('/carrito', postCarrito);
+
+router.delete('/carrito', deleteCarrito);
+
+router.get('/productos/:id', detalleProducto);
+
+router.get('/orden_de_compra', getOrden);
+
+router.post('/orden_de_compra', postOrden);
+
+router.put('/orden_de_compra', putOrden);
+
+router.delete('/orden_de_compra', deleteOrden);
+
+router.get('/factura', getFactura);
+
+router.post('/factura', postFactura);
+
+router.put('/factura', putFactura);
+
+router.delete('/factura', deleteFactura);
+
+router.get('/cliente', getCliente);
+
+router.post('/cliente', postCliente);
+
+router.put('/cliente', putCliente);
+
+router.delete('/cliente', deleteCliente);
 
 
 
